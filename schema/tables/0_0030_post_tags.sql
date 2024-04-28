57542d3190b0761e9a3468594bf4ef50aa194090
David Moura <david.dbmoura@gmail.com>
2024-04-28 17:45:23 +0200

    remove forein key from table creation

--- schema/tables/0_0030_post_tags.sql
+++ schema/tables/0_0030_post_tags.sql
@@ -1,7 +1,5 @@
 CREATE TABLE IF NOT EXISTS Post_Tags (
     post_id INTEGER NOT NULL,
     tag_id INTEGER NOT NULL,
-    PRIMARY KEY (post_id, tag_id),
-    FOREIGN KEY (post_id) REFERENCES Posts(id),
-    FOREIGN KEY (tag_id) REFERENCES Tags(id)
+    PRIMARY KEY (post_id, tag_id)
 );
