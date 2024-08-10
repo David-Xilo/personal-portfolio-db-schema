d44d4121b0af15ee7ab5f7a3d860a3d0e1b60b88
David Moura <david.dbmoura@gmail.com>
2024-08-10 11:52:39 +0200

    Fix tables names

--- schema/constraints/2_0010_foreign_keys.sql
+++ schema/constraints/2_0010_foreign_keys.sql
@@ -1,14 +1,14 @@
 -- Foreign key constraints for Posts
 ALTER TABLE BLOG_POSTS
-ADD CONSTRAINT fk_posts_user_id FOREIGN KEY (user_id) REFERENCES Users(id),
-ADD CONSTRAINT fk_posts_category_id FOREIGN KEY (category_id) REFERENCES Categories(id);
+ADD CONSTRAINT fk_posts_user_id FOREIGN KEY (user_id) REFERENCES USERS(id),
+ADD CONSTRAINT fk_posts_category_id FOREIGN KEY (category_id) REFERENCES CATEGORIES(id);
 
 -- Foreign key constraints for Comments
 ALTER TABLE COMMENTS
-ADD CONSTRAINT fk_comments_post_id FOREIGN KEY (post_id) REFERENCES Posts(id),
-ADD CONSTRAINT fk_comments_user_id FOREIGN KEY (user_id) REFERENCES Users(id);
+ADD CONSTRAINT fk_comments_post_id FOREIGN KEY (post_id) REFERENCES BLOG_POSTS(id),
+ADD CONSTRAINT fk_comments_user_id FOREIGN KEY (user_id) REFERENCES USERS(id);
 
 -- Foreign key constraints for Post_Tags
 ALTER TABLE POST_TAGS
-ADD CONSTRAINT fk_post_tags_post_id FOREIGN KEY (post_id) REFERENCES Posts(id),
-ADD CONSTRAINT fk_post_tags_tag_id FOREIGN KEY (tag_id) REFERENCES Tags(id);
+ADD CONSTRAINT fk_post_tags_post_id FOREIGN KEY (post_id) REFERENCES BLOG_POSTS(id),
+ADD CONSTRAINT fk_post_tags_tag_id FOREIGN KEY (tag_id) REFERENCES TAGS(id);
