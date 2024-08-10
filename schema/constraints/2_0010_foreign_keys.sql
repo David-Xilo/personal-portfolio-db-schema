3e53299c492768b8a5d0849e8b41c061e21ab2f8
David Moura <david.dbmoura@gmail.com>
2024-08-10 11:47:33 +0200

    change tables names

--- schema/constraints/2_0010_foreign_keys.sql
+++ schema/constraints/2_0010_foreign_keys.sql
@@ -1,14 +1,14 @@
 -- Foreign key constraints for Posts
-ALTER TABLE Posts
+ALTER TABLE BLOG_POSTS
 ADD CONSTRAINT fk_posts_user_id FOREIGN KEY (user_id) REFERENCES Users(id),
 ADD CONSTRAINT fk_posts_category_id FOREIGN KEY (category_id) REFERENCES Categories(id);
 
 -- Foreign key constraints for Comments
-ALTER TABLE Comments
+ALTER TABLE COMMENTS
 ADD CONSTRAINT fk_comments_post_id FOREIGN KEY (post_id) REFERENCES Posts(id),
 ADD CONSTRAINT fk_comments_user_id FOREIGN KEY (user_id) REFERENCES Users(id);
 
 -- Foreign key constraints for Post_Tags
-ALTER TABLE Post_Tags
+ALTER TABLE POST_TAGS
 ADD CONSTRAINT fk_post_tags_post_id FOREIGN KEY (post_id) REFERENCES Posts(id),
 ADD CONSTRAINT fk_post_tags_tag_id FOREIGN KEY (tag_id) REFERENCES Tags(id);
--- schema/tables/1_0060_users.sql
+++ schema/tables/1_0060_users.sql
@@ -1,8 +1,8 @@
-CREATE TYPE user_role AS ENUM ('admin', 'editor', 'regular');
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TYPE USER_ROLE AS ENUM ('admin', 'editor', 'regular');
+CREATE TABLE IF NOT EXISTS USERS (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    role user_role NOT NULL DEFAULT 'regular'
+    role USER_ROLE NOT NULL DEFAULT 'regular'
 );
