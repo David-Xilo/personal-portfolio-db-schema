8a11249cb2cf1edecf123245347df63b53be133f
David Moura <david.dbmoura@gmail.com>
2025-06-10 20:24:54 +0200

    change values

--- schema/data_changes/3_0010-insert_placeholder.sql
+++ schema/data_changes/3_0010-insert_placeholder.sql
@@ -1,13 +1,13 @@
 INSERT INTO CONTACTS (name, email, linkedin, github, active)
-VALUES ('John Doe from DB', 'johndoe@example.com', 'https://www.linkedin.com/in/johndoe', 'https://github.com/johndoe', true);
+VALUES ('Xillz', 'anon.casual.human@proton.me', 'https://www.linkedin.com/in/johndoe', 'https://github.com/johndoe', true);
 
-insert into GAMES (title, genre, rating, description, link_to_store, link_to_git)
-values ('game title from DB', 'strategy', null, 'game description', 'https://github.com/game', 'https://github.com/johndoe');
-
-insert into GAMES (title, genre, rating, description, link_to_store, link_to_git)
-values ('game with rating title from DB', 'table top', 3, 'game with rating description', 'https://github.com/othergame', null);
+-- insert into GAMES (title, genre, rating, description, link_to_store, link_to_git)
+-- values ('game title from DB', 'strategy', null, 'game description', 'https://github.com/game', 'https://github.com/johndoe');
+--
+-- insert into GAMES (title, genre, rating, description, link_to_store, link_to_git)
+-- values ('game with rating title from DB', 'table top', 3, 'game with rating description', 'https://github.com/othergame', null);
 
 
 insert into TECH_PROJECTS (title, description, link_to_git)
-values ('tech project title from DB', 'project description', 'https://github.com/johndoe');
+values ('Safehouse', 'A basic personal website. Has the tech portfolio', 'https://github.com/johndoe');
 
