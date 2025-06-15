7cbacfe05668e5c091472163b995c69843633df5
David Moura <david.dbmoura@gmail.com>
2025-06-15 15:49:53 +0200

    add games played

--- schema/data_changes/3_0010-insert_placeholder.sql
+++ schema/data_changes/3_0010-insert_placeholder.sql
@@ -11,3 +11,15 @@ VALUES ('Xillz', 'anon.casual.human@proton.me', 'https://www.linkedin.com/in/joh
 insert into TECH_PROJECTS (title, description, link_to_git)
 values ('Safehouse', 'A basic personal website. Has the tech portfolio', 'https://github.com/johndoe');
 
+
+insert into GAMES_PLAYED (title, genre, rating, description)
+values ('Skyrim', 'rpg', 5, 'A sprawling, snow-drenched Nordic realm to freely explore, brimming with epic dragon battles, rich lore, unforgettable quests, dynamic combat, and endless mod-friendly adventures.');
+
+insert into GAMES_PLAYED (title, genre, rating, description)
+values ('DnD', 'table top', 5, 'Dungeons & Dragons is a boundless fantasy role-playing game full of imaginative storytelling, camaraderie, problem-solving, and epic quests—empowering creativity, teamwork, and adventure at every roll of the dice.');
+
+insert into GAMES_PLAYED (title, genre, rating, description)
+values ('Age of Empires II', 'strategy', 4, 'Age of Empires II is a legendary real-time strategy game set in the Middle Ages—build thriving empires across 13 civilizations, master resource economy, epic battles, historic campaigns, and deep multiplayer—timeless classic');
+
+insert into GAMES_PLAYED (title, genre, rating, description)
+values ('Final Fantasy 7', 'rpg', 4, 'Final Fantasy VII is a timeless, genre‑defining RPG—rich with unforgettable characters, sweeping eco‑drama, cinematic twists, iconic music, and deep, emotion‑driven storytelling that still enthralls');
