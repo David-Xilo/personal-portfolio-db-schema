38b555ea05ba779f13c6729163f0cc1ee49d10c2
David Bugalho de Moura <David-Xilo@users.noreply.github.com>
2025-07-18 21:33:36 +0200

    fix paths

--- schema/googlecloud/run-migrations-postgres.sh
+++ schema/googlecloud/run-migrations-postgres.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# run-migrations-postgres.sh
+
 DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}?sslmode=disable"
 migrate -path /migrations -database "$DATABASE_URL" "${1:-up}"
 
