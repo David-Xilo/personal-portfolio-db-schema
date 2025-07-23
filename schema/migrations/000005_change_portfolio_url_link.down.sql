1510de4efd08fb857b96c1b4a1fb8f10315246d4
David Bugalho de Moura <David-Xilo@users.noreply.github.com>
2025-07-23 10:05:21 +0200

    change portfolio link

---
+++ schema/migrations/000005_change_portfolio_url_link.down.sql
@@ -0,0 +1,6 @@
+
+UPDATE PROJECT_GROUPS
+SET LINK_TO_PROJECT = 'https://davidmoura.net'
+WHERE description = 'A basic personal portfolio website' AND project_type = 'tech';
+
+
---
+++ schema/migrations/000005_change_portfolio_url_link.up.sql
@@ -0,0 +1,6 @@
+
+UPDATE PROJECT_GROUPS
+SET LINK_TO_PROJECT = 'https://www.davidmoura.net'
+WHERE description = 'A basic personal portfolio website' AND project_type = 'tech';
+
+
