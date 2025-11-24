9dc1b79860cde83ba1acbb9331813a852c1bb39f
David Bugalho de Moura <David-Xilo@users.noreply.github.com>
2025-11-24 19:56:19 +0000

    cleanup

--- schema/googlecloud/run-migrations-postgres.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}?sslmode=disable"
-migrate -path /migrations -database "$DATABASE_URL" "${1:-up}"
-
